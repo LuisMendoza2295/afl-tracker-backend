5708367bf7553680c99e02e0261eb8aa1449911e
Luis Mendoza <luismendoza2295@gmail.com>
2026-01-04 12:24:14 -0500

    Setup pulumi (#1)
    * Add migration plan and progress files for setup-pulumi branch
    
    * setup-pulumi: add pulumi hello world config
    
    * setup-pulumi: move .gitignore to root folder
    
    * setup-pulumi: .gitignore per each subfolder
    
    * setup-pulumi: remove quarkus generated code from root folder
    
    * setup-pulumi: remove unused import
    
    * setup-pulumi: .gitignore at root level
    
    * setup-pulumi: clean staged ignored folders

---
+++ pulumi-infra/src/main/java/com/afl/tracker/App.java
@@ -0,0 +1,17 @@
+package com.afl.tracker;
+
+import com.pulumi.Pulumi;
+import com.pulumi.gcp.storage.Bucket;
+import com.pulumi.gcp.storage.BucketArgs;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(ctx -> {
+            var bucket = new Bucket("my-bucket",
+                                    BucketArgs.builder()
+                                    .location("US")
+                                    .build());
+            ctx.export("bucketName", bucket.url());
+        });
+    }
+}
