c283d5c6800ad1d7e1c8efa483d51d76a3d897d5
Luis Mendoza <luismendoza2295@gmail.com>
2026-01-20 02:41:06 -0500

    main: allow unauthenticated

--- pulumi-infra/src/main/java/com/afl/tracker/CloudRun.java
+++ pulumi-infra/src/main/java/com/afl/tracker/CloudRun.java
@@ -1,5 +1,7 @@
 package com.afl.tracker;
 
+import java.util.Map;
+
 import com.pulumi.Context;
 import com.pulumi.core.Output;
 import com.pulumi.gcp.cloudrunv2.Service;
@@ -19,6 +21,7 @@ public class CloudRun {
   public static Service createCloudRunService(Context ctx, Output<String> image, Output<String> runtimeSAEmail, Output<String> vpcName, Output<String> privateSubnetName) {
     String serviceName = ctx.config().require("cloudRunServiceName");
     String region = ctx.config("gcp").require("region");
+    String projectId = ctx.config("gcp").require("project");
     var cloudRunService = new Service(serviceName,
         ServiceArgs.builder()
             .name(serviceName)
@@ -33,6 +36,9 @@ public class CloudRun {
                         .build())
                     .egress("PRIVATE_RANGES_ONLY")
                     .build())
+                .annotations(Map.of(
+                    "run.googleapis.com/invoker-iam-disabled", "true"
+                ))
                 .scaling(ServiceTemplateScalingArgs.builder()
                     .maxInstanceCount(2)
                     .minInstanceCount(1)
@@ -51,6 +57,7 @@ public class CloudRun {
             .build());
 
     new ServiceIamMember("allow-public-access", ServiceIamMemberArgs.builder()
+        .project(projectId)
         .location(cloudRunService.location())
         .name(cloudRunService.name())
         .role("roles/run.invoker")
